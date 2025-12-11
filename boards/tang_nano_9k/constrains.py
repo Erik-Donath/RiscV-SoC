7171ba3652423128be5bbe09a986e2142da04da6
Erik Donath <erikdonath@proton.me>
2025-12-11 19:42:37 +0100

    Removed constrains.py and added all pin definitions of tang nano 9k

--- boards/tang_nano_9k/constrains.py
+++
@@ -1,9 +0,0 @@
-"""Tang Nano 9K Timing Constraints"""
-
-# Currently timing constraints are handled in platform.py
-# This file is reserved for future advanced constraint definitions
-
-# Example for future use:
-# def add_constraints(platform):
-#     """Add advanced timing constraints"""
-#     pass
